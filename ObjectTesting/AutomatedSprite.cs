6b0c433285704660138a6dc60d7e419b00644f51
MrCythos <johndoeacc.false@gmail.com>
2016-01-28 11:50:43 -0600

    Basic info and crap

---
+++ ObjectTesting/AutomatedSprite.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics; 
+
+namespace ObjectTesting
+{
+    class AutomatedSprite:Sprite
+    {
+        public AutomatedSprite(Texture2D textureImage, Vector2 position, Point frameSize, int collisionOffset, Point currentFrame, Point sheetSize, Vector2 speed) 
+            : base(textureImage, position, frameSize, collisionOffset, currentFrame, sheetSize, speed) //uses default animation speed
+        { 
+
+        }
+
+        public AutomatedSprite(Texture2D textureImage, Vector2 position, Point frameSize, int collisionOffset, Point currentFrame, Point sheetSize, Vector2 speed, int millisecondsPerFrame) 
+            : base(textureImage, position, frameSize, collisionOffset, currentFrame, sheetSize, speed, millisecondsPerFrame) //uses specific animation speed
+        { 
+
+        }
+
+        public override Vector2 direction 
+        { 
+            get 
+            { 
+                return speed; 
+            } 
+        }
+
+        public override void Update(GameTime gameTime, Rectangle clientBounds)
+        {
+            position += direction; //simply moves in whatever speed is specified in the constructor
+            base.Update(gameTime, clientBounds);
+        }
+
+
+
+    }
+}
---
+++ ObjectTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ObjectTesting")]
+[assembly: AssemblyProduct("ObjectTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("4c44f0d8-5af1-49ef-9fd2-b96f0815d189")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
